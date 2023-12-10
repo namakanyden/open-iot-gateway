1419552b0d9a2f1b88fc88c5fd3e075187f8bb5f
mirek <mirek@cnl.sk>
2023-12-10 20:55:58 +0100

    refaktoring, pridanie restartu po skonceni skriptu

--- project/iotgw/scripts/provisioning.rpi3.bash
+++ project/iotgw/scripts/provisioning.rpi3.bash
@@ -61,10 +61,10 @@ function setup_system() {
 function setup_wifi_ap() {
     log "Setup WiFi AP"
 
-    nmcli connection add type wifi ifname wlan0 con-name Hotspot autoconnect yes ssid "${ROOM}-things"
+    nmcli connection add type wifi ifname wlan0 con-name Hotspot autoconnect yes ssid "${_ROOM}-things"
     nmcli connection modify Hotspot 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
     nmcli connection modify Hotspot wifi-sec.key-mgmt wpa-psk
-    nmcli connection modify Hotspot wifi-sec.psk "welcome.to.the.${ROOM}"
+    nmcli connection modify Hotspot wifi-sec.psk "welcome.to.the.${_ROOM}"
     nmcli connection up Hotspot
 }
 
@@ -79,7 +79,10 @@ function main() {
     setup_wifi_ap
     setup_maker
 
-    log "Done"
+    log "Reboot in 10 seconds..."
+    sleep 10
+    log "Rebooting now..."
+    reboot
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
