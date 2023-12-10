e39acbd65693aea4529b32d02bfdaaf1f9e3a187
mirek <mirek@cnl.sk>
2023-12-10 09:34:45 +0100

    refaktoring

--- project/iotgw/scripts/provisioning.rpi3.bash
+++ project/iotgw/scripts/provisioning.rpi3.bash
@@ -4,14 +4,31 @@ set -o errexit  # stop when error occurs
 set -o pipefail # if not, expressions like `error here | true` will always succeed
 set -o nounset  # detects uninitialised variables
 
-readonly _PACKAGES="vim btop dnsmasq hostapd"
+readonly _PACKAGES="vim btop dnsmasq hostapd podman git tmux"
 readonly _USERNAME="maker"
 readonly _PASSWORD="rekam"
 readonly _ROOM="caprica"
+readonly _OPEN_IOT_GW_URL="https://github.com/namakanyden/open-iot-gateway"
 
+readonly _HOSTAPD_CONF="
+country_code=SK
+interface=wlan0
+ssid=${_ROOM}-things
+wpa_passphrase=welcome.to.the.${_ROOM}
+driver=nl80211
+hw_mode=g
+channel=6
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_key_mgmt=WPA-PSK
+rsn_pairwise=CCMP
+"
 
 # functions
-function log(){
+function log() {
     local message="${*}"
     local now
     now=$(date +%H:%M:%S)
@@ -19,26 +36,30 @@ function log(){
     printf "\e[0;35m%s\e[m: \e[0;33m%s\e[m\n" "${now}" "${message}"
 }
 
+function install_software() {
+    log "Software installation"
 
-function install_software(){
-  log "Software installation"
+    apt update && sudo apt upgrade --yes
+    apt install --yes ${_PACKAGES}
 
-  apt update && sudo apt upgrade
-  apt install ${_PACKAGES}
-
-  # install docker
-  curl -sSL https://get.docker.io | sh
+    # install docker
+    #   log "Installing Docker"
+    #   curl -sSL https://get.docker.io | sh
 }
-
 
 function setup_maker() {
-  log "Create and setup maker user"
+    log "Create and setup maker user"
 
-  useradd --password "${_PASSWORD}" --user-group --groups docker "${_USERNAME}"
-  chpasswd <<< "${_USERNAME}:${_PASSWORD}"
+    if [[ ! $(id $_USERNAME > /dev/null 2>&1) ]]; then
+        log "User maker already exists."
+        return 0
+    fi
+
+    useradd --password "${_PASSWORD}" --user-group "${_USERNAME}"
+    chpasswd <<<"${_USERNAME}:${_PASSWORD}"
 }
 
-function setup_system(){
+function setup_system() {
     log "System Setup."
 
     # set the hostname based on the name of the room
@@ -46,33 +67,32 @@ function setup_system(){
 
 }
 
-
 # sources:
 # https://raspberrypi-guide.github.io/networking/create-wireless-access-point
 function setup_wifi_ap() {
-  log "Setup WiFi AP"
+    log "Setup WiFi AP"
 
-  # stop service with no proper configuration
-  systemctl stop dnsmasq
-  systemctl stop hostapd
+    # stop service with no proper configuration
+    systemctl stop dnsmasq
+    systemctl stop hostapd
 
-  # set DHCP server static IP address
-  cat <<- 'EOF' > "/etc/dhcpd.conf"
+    # set DHCP server static IP address
+    cat <<-'EOF' >"/etc/dhcpd.conf"
 interface wlan0
     static ip_address=192.168.4.1/24
     nohook wpa_supplicant
 EOF
-  systemctl restart dhcpcd
+    systemctl restart dhcpcd
 
-  # setup DHCP server
-  cat <<- 'EOF' > "/etc/dnsmasq.conf"
+    # setup DHCP server
+    cat <<-'EOF' >"/etc/dnsmasq.conf"
 interface=wlan0
 dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
 EOF
-  systemctl restart dnsmasq
+    systemctl restart dnsmasq
 
-  # setup hostapd
-  printf "
+    # setup hostapd
+    printf "
 country_code=SK
 interface=wlan0
 ssid=%s-things
@@ -87,31 +107,28 @@ ignore_broadcast_ssid=0
 wpa=2
 wpa_key_mgmt=WPA-PSK
 rsn_pairwise=CCMP
-" "${_ROOM}" "${_ROOM}" > /etc/hostapd/hostapd.conf
+" "${_ROOM}" "${_ROOM}" >/etc/hostapd/hostapd.conf
 
-  sed -e '/DAEMON_CONF/d' -e '$aDAEMON_CONF="/etc/hostapd/hostapd.conf"' -i /etc/default/hostapd
+    sed -e '/DAEMON_CONF/d' -e '$aDAEMON_CONF="/etc/hostapd/hostapd.conf"' -i /etc/default/hostapd
 
-  # run
-  systemctl unmask hostapd
-  systemctl enable hostapd
-  systemctl start hostapd
+    # run
+    systemctl unmask hostapd
+    systemctl enable hostapd
+    systemctl start hostapd
 }
 
+function main() {
+    if [[ $UID != 0 ]]; then
+        printf "ERROR: Need to be root.\n" >&2
+        exit 1
+    fi
 
-function main(){
-  if [[ $UID != 0 ]]; then
-    printf "ERROR: Need to be root."
-    exit 1
-  fi
+    install_software
+    setup_system
+    setup_wifi_ap
+    setup_maker
 
-  install_software
-  setup_system
-  setup_wifi_ap
-  setup_maker
-
-  log "Done"
+    log "Done"
 }
-
 
 main
-
