e4d9bc890b6433b8687b79de240cd30b6af84661
Domino <dominik.9.gajdos@gmail.com>
2024-01-31 15:06:20 +0100

    wip

--- project/mother/configs/bin/processor.py
+++ project/mother/configs/bin/processor.py
@@ -43,9 +43,11 @@ def main():
 
             department, room, device_type, device_id = topic.split("/")
             device_id = device_id.replace(" ", "\ ")
-            measurement = 'metrics'
-            if device_id == 'thing':
+            measurement = 'measurements'
+            if device_type == 'thing':
                 measurement = 'things'
+            if device_type == 'metrics':
+                measurement = 'metrics'
 
             validate_data(schemas, device_type, fields)
             ts = fields.pop("ts")
