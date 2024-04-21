0b876a5deaa496b963a4faacf3ecd244e02d374d
podmepodme <petergrocky23@gmail.com>
2024-04-21 11:18:21 +0200

    fix:

--- project/iotgw/chaos/core/device_monitor.py
+++ project/iotgw/chaos/core/device_monitor.py
@@ -1,15 +1,21 @@
+import logging
+
 import pyudev
 import threading
+import signal
+import os
 
 
 class DeviceMonitor:
     def __init__(self, subsystem):
+        self.logger = logging.getLogger("usb")
         self.subsystem_to_monitor = subsystem
 
         self.__connect_callbacks = []
         self.__disconnect_callbacks = []
 
         self.__monitor_thread = threading.Thread(target=self.__monitor)
+        self.__monitor_thread.daemon = True
         self.__monitor_thread.start()
 
     def connect(self, func):
@@ -21,12 +27,19 @@ class DeviceMonitor:
         return func
 
     def __monitor(self):
-        context = pyudev.Context()
-        monitor = pyudev.Monitor.from_netlink(context)
-        monitor.filter_by(subsystem=self.subsystem_to_monitor)
+        try:
+            context = pyudev.Context()
+            monitor = pyudev.Monitor.from_netlink(context)
+            monitor.filter_by(subsystem=self.subsystem_to_monitor)
 
-        for device in iter(monitor.poll, None):
-            self.call_decorated(device)
+            for device in iter(monitor.poll, None):
+                self.call_decorated(device)
+        except ImportError:
+            self.logger.critical("udev is not installed or supported on this device.")
+            os._exit(1)
+        except SystemExit:
+            self.logger.critical("udev is not installed or supported on this device.")
+            os._exit(1)
 
     def call_decorated(self, device):
         if device.action == "remove":
