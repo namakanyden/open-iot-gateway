0686aaf8d000f2794c944d80bc77a1e1afa8dd7f
podmepodme <petergrocky23@gmail.com>
2024-04-21 12:05:05 +0200

    feat: debug info for devices

--- project/iotgw/chaos/services/usb_checker.py
+++ project/iotgw/chaos/services/usb_checker.py
@@ -6,16 +6,20 @@ from __main__ import mqtt
 logger = logging.getLogger("usb")
 
 
+def device_info(device):
+    for attr in list(vars(device)):
+        logger.debug(attr)
+
+
 @usb.connect
 def print_connect_message(device):
     logger.critical("USB Device connected...")
-    mqtt.publish("gateway/chaos/device-manager", "connected")
-    mqtt.publish("gateway/chaos/device-manager", device.device_path)
-    logger.debug(device)
+    logger.debug(device_info(device))
+    mqtt.publish("gateway/chaos/device-manager", f"connected {device.device_path}")
 
 
 @usb.disconnect
 def print_disconnect_message(device):
     logger.critical("USB Device disconnected...")
+    logger.debug(device_info(device))
     mqtt.publish("gateway/chaos/device-manager", f"disconnected {device.device_path}")
-    logger.debug(device)
